08ba5a43ed680dfafe3f0c946547b04a6225f9ed
Jay <jhd@fastmail.net>
2015-04-02 16:41:50 -0400

    Prevent automatic creation of helpers, assets, view specs

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,11 @@ module StockTalk
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |generate|
+        generate.helper false
+        generate.assets false
+        generate.view_specs false
+    end
   end
 end
