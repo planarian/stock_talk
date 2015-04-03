400240e75e6893ebeb4dad16a19759ede98a9afa
Jay <jhd@fastmail.net>
2015-04-03 15:35:42 -0400

    Create test landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :companies, only: [:index, :show]
+
+  root 'companies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
