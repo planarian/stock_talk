b47eefe1992e82da3ba4d958d1058cf585cf701c
Jay <jhd@fastmail.net>
2015-04-03 14:30:12 -0400

    Add factories, empty tests for models

---
+++ spec/factories/daily_totals.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :daily_total do
+    count "MyString"
+ ""
+time "MyString"
+ "2015-04-03 14:13:15"
+company_id "MyString"
+ ""
+  end
+
+end
---
+++ spec/factories/queries.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :query do
+    last_tweet "MyString"
+ ""
+count "MyString"
+ ""
+time "MyString"
+ "2015-04-03 14:15:55"
+ ""
+int "MyString"
+  end
+
+end
