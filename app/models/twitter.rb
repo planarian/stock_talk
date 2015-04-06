4e7cbf98ff3b9bacea732453aca16401bfa22bb1
Jay <jhd@fastmail.net>
2015-04-06 12:54:52 -0400

    Rewrite to use max_id

--- app/models/twitter.rb
+++ app/models/twitter.rb
@@ -3,46 +3,55 @@ require 'csv'
 module RetrieveTweets
   
   def self.get(company, *start_id)
-    qty = message_wrapper(company.name) { store(company, ask(company, start_id)) }
-    qty = message_wrapper(company.name) { store(company, ask(company)) } while qty == 100
+    print "Downloading tweets for #{name}..."
+    store(company, ask(company, start_id))
+    print "done. #{qty} tweet(s) retrieved.\n"
     ToCSV.convert(Tweet, "tweet.csv")
   end
 
-  def self.message_wrapper(name)
-    print "Downloading tweets for #{name}..."
-    qty = yield 
-    print "done. #{qty} tweet(s) retrieved.\n"
-    qty
-  end
-
   def self.ask(company, *start_id)
-      start_id = start_id[0] unless start_id.empty?
-      unless company.queries.count.zero?
-        return Client.search(
-          company.name, since_id: start_id.empty? ? company.queries.last.most_recent_tweet : start_id[0])
+    unless company.queries.empty?
+      begin_id = start_id.flatten.empty? ? company.queries.last.most_recent_tweet : start_id.flatten[0]
+      results = Client.search(company.name, since_id: begin_id).attrs[:statuses]
+    else
+      if start_id.flatten.empty?
+        results = Client.search(company.name).attrs[:statuses]
       else 
-        return start_id.empty? ? Client.search(company.name) : Client.search(company.name, start_id[0])
+        begin_id = start_id.flatten[0]
+        results = Client.search(company.name, since_id: begin_id).attrs[:statuses]
+        puts "batch 1"
       end
+    end
+    
+    if results.count == 100
+      count = 1
+      loop do 
+        interm_results = Client.search(company.name, 
+          {max_id: results.last[:id], since_id: begin_id}).attrs[:statuses]
+        results += interm_results
+        puts "batch #{count}. #{interm_results.count} added. #{results.count} total."
+        break if interm_results.count < 100
+      end
+    end
+    results
   end
 
   def self.store(company, results)
-    qty = results.attrs[:statuses].count
-    puts "\nbegin: #{results.attrs[:statuses].last[:id]}\nend: #{results.attrs[:statuses].first[:id]}\n" unless qty.zero?
-    
+    qty = results.count
     unless qty.zero?
+      puts "\nbegin: #{results.attrs[:statuses].last[:id]}\nend: #{results.attrs[:statuses].first[:id]}\n"  
       query = company.queries.create(
                  time: Time.now,
                  count: qty,
-                 earliest_tweet: results.attrs[:statuses].last[:id],
-                 most_recent_tweet: results.attrs[:statuses].first[:id])
+                 earliest_tweet: results.last[:id],
+                 most_recent_tweet: results.first[:id])
       store_tweets(query, results)
     end
-    
     qty
   end
 
   def self.store_tweets(query, results)
-    results.attrs[:statuses].each do |record|
+    results.each do |record|
       query.tweets.create(
         number: record[:id],
         text: record[:text],
@@ -54,7 +63,7 @@ end
 
 module ToCSV
   def self.convert(model, filename)
-    CSV.open("#{filename}", "wb") do |csv|
+    CSV.open(filename, "wb") do |csv|
       csv << model.attribute_names
       model.all.each do |item| 
         csv << item.attributes.values.map{ |x| x.instance_of?(BigDecimal) ? x.to_i.to_s : x }
