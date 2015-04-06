6c9fdb6ec6428de0fffc26fdb3f8ac0d9f934158
Jay <jhd@fastmail.net>
2015-04-06 09:23:02 -0400

    Refactor

--- app/models/twitter.rb
+++ app/models/twitter.rb
@@ -2,30 +2,55 @@ require 'csv'
 
 module RetrieveTweets
   
-  def self.get(company)
-    store(company, ask(company))
+  def self.get(company, *start_id)
+    qty = message_wrapper(company.name) { store(company, ask(company, start_id)) }
+    qty = message_wrapper(company.name) { store(company, ask(company)) } while qty == 100
+    ToCSV.convert(Tweet, "tweet.csv")
   end
 
-  private
+  def self.message_wrapper(name)
+    print "Downloading tweets for #{name}..."
+    qty = yield 
+    print "done. #{qty} tweet(s) retrieved.\n"
+    qty
+  end
 
-  def self.ask(company)
+  def self.ask(company, *start_id)
+      start_id = start_id[0] unless start_id.empty?
       unless company.queries.count.zero?
-        return Client.search(company.name, since_id: 584739789141839872)
+        return Client.search(
+          company.name, since_id: start_id.empty? ? company.queries.last.most_recent_tweet : start_id[0])
       else 
-        return Client.search(company.name)
+        return start_id.empty? ? Client.search(company.name) : Client.search(company.name, start_id[0])
       end
   end
 
   def self.store(company, results)
     qty = results.attrs[:statuses].count
     puts "\nbegin: #{results.attrs[:statuses].last[:id]}\nend: #{results.attrs[:statuses].first[:id]}\n" unless qty.zero?
-    company.queries.create(time: Time.now,
-                           count: qty,
-                           earliest_tweet: results.attrs[:statuses].last[:id],
-                           most_recent_tweet: results.attrs[:statuses].first[:id]) unless qty.zero?
+    
+    unless qty.zero?
+      query = company.queries.create(
+                 time: Time.now,
+                 count: qty,
+                 earliest_tweet: results.attrs[:statuses].last[:id],
+                 most_recent_tweet: results.attrs[:statuses].first[:id])
+      store_tweets(query, results)
+    end
+    
     qty
   end
+
+  def self.store_tweets(query, results)
+    results.attrs[:statuses].each do |record|
+      query.tweets.create(
+        number: record[:id],
+        text: record[:text],
+        time: record[:created_at] )
+    end
+  end
 end
+
 
 module ToCSV
   def self.convert(model, filename)
