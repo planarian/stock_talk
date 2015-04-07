bc095e45a256b5b92a3557b139dfa87cce96faad
Jay <jhd@fastmail.net>
2015-04-07 14:50:17 -0400

    Change block from 'do' to {}

--- lib/to_csv.rb
+++ lib/to_csv.rb
@@ -5,9 +5,7 @@ module ToCSV
   def self.convert(model, filename)
     CSV.open(filename, "wb") do |csv|
       csv << model.attribute_names
-      model.all.each do |item| 
-        csv << item.to_a
-      end
+      model.all.each { |item| csv << item.to_a }
     end
   end
 end
