b5ce02fbb31aa820d21416bfb6ea059556f45c7a
Jay <jhd@fastmail.net>
2015-04-07 11:22:01 -0400

    Use constant for tweet download limit

--- app/models/twitter.rb
+++ app/models/twitter.rb
@@ -1,6 +1,7 @@
 require "#{Rails.root}/lib/to_csv"
 
 module RetrieveTweets
+  MAX_COUNT = 100
   
   def self.get(company)
     print "Downloading tweets for #{name}..."
@@ -12,18 +13,18 @@ module RetrieveTweets
   def self.ask(company)
     unless company.queries.empty?
       min_id = company.queries.last.most_recent_tweet
-      results = Client.search(company.name, {count: 100, since_id: min_id}).attrs[:statuses]
+      results = Client.search(company.name, {count: MAX_COUNT, since_id: min_id}).attrs[:statuses]
     else
-      results = Client.search(company.name, count: 100).attrs[:statuses]
+      results = Client.search(company.name, count: MAX_COUNT).attrs[:statuses]
     end
     
-    if min_id && results.count == 100
+    if min_id && results.count == MAX_COUNT
       loop do 
         interm_results = Client.search(company.name, 
-          {count: 100, max_id: results.last[:id] - 1, since_id: min_id}).attrs[:statuses]
+          {count: MAX_COUNT, max_id: results.last[:id] - 1, since_id: min_id}).attrs[:statuses]
         results += interm_results
         puts "#{interm_results.count} added. #{results.count} total."
-        break if interm_results.count < 100
+        break if interm_results.count < MAX_COUNT
       end
     end
     results
