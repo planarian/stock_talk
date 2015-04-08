04337ae2d5a94de6cc844285fe35817c495e6291
Jay <jhd@fastmail.net>
2015-04-08 11:07:19 -0400

    Add tweet id

--- db/migrate/20150403181205_create_companies.rb
+++ db/migrate/20150403181205_create_companies.rb
@@ -2,6 +2,7 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.string :name, null: false
+      t.decimal :most_recent_tweet
       t.timestamps null: false
     end
   end
