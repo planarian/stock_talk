e9b2b72c0df3b38dc6e726598cd6b103b0726beb
Jay <jhd@fastmail.net>
2015-04-08 11:02:51 -0400

    Add tweet id field

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,18 +6,4 @@ class Company < ActiveRecord::Base
   validates :name, presence: true
 
 
-  def consolidate
-    queries.group_by { |q| q.time.to_date }.each do |key, val|
-      curr_day = Day.find_by(date: key)
-      new_total = val.inject(0) { |acc, nxt| acc + nxt.count }
-      already_stored = daily_totals.find_by(day_id: curr_day)
-      if already_stored
-        already_stored.update(count: already_stored.count + new_total)
-      else
-        daily_totals.create!(day_id: curr_day.id, count: new_total)
-        #Why doesn't 'day_id: curr_day' work here?
-      end
-    end
-    queries.destroy_all
-  end
 end
--- spec/factories/tweets.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :tweet do
-    number "9.99"
-text "MyString"
-time "2015-04-05 12:18:10"
-  end
-
-end
