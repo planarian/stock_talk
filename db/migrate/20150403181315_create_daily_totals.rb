a9fdf82c2161f675db3be551ee70a7c6a71dcc53
Jay <jhd@fastmail.net>
2015-04-07 22:26:05 -0400

    Add error and tweet-number fields

--- db/migrate/20150403181315_create_daily_totals.rb
+++ db/migrate/20150403181315_create_daily_totals.rb
@@ -2,6 +2,8 @@ class CreateDailyTotals < ActiveRecord::Migration
   def change
     create_table :daily_totals do |t|
       t.integer :count, null: false
+      t.decimal :most_recent_tweet, null: false
+      t.boolean :error, default: false, null: false
       t.integer :company_id, null: false
       t.integer :day_id, null: false
       t.timestamps
