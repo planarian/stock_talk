c7dba4e780781521cbdd9e701f76bcd1dbdf05df
Jay <jhd@fastmail.net>
2015-04-08 11:54:41 -0400

    Reflects new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407024418) do
+ActiveRecord::Schema.define(version: 20150408144144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",              null: false
+    t.decimal  "most_recent_tweet"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "daily_totals", force: :cascade do |t|
-    t.integer  "count",      null: false
-    t.integer  "company_id", null: false
-    t.integer  "day_id",     null: false
+    t.integer  "count",                      null: false
+    t.boolean  "error",      default: false, null: false
+    t.integer  "company_id",                 null: false
+    t.integer  "day_id",                     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -34,19 +36,13 @@ ActiveRecord::Schema.define(version: 20150407024418) do
     t.date "date", null: false
   end
 
-  create_table "queries", force: :cascade do |t|
-    t.decimal  "earliest_tweet",    null: false
-    t.decimal  "most_recent_tweet", null: false
-    t.integer  "count",             null: false
-    t.datetime "time",              null: false
-    t.integer  "company_id",        null: false
-  end
-
-  create_table "tweets", force: :cascade do |t|
-    t.decimal  "number",   null: false
-    t.string   "text",     null: false
-    t.datetime "time",     null: false
-    t.integer  "query_id", null: false
+  create_table "share_prices", force: :cascade do |t|
+    t.decimal  "price"
+    t.boolean  "error",      default: false, null: false
+    t.integer  "day_id",                     null: false
+    t.integer  "company_id",                 null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
