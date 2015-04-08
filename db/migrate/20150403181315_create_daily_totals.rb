435a55b1d59e5df27648f1f0fa3dad956315c6e9
Jay <jhd@fastmail.net>
2015-04-08 11:08:57 -0400

    Add error field

--- db/migrate/20150403181315_create_daily_totals.rb
+++ db/migrate/20150403181315_create_daily_totals.rb
@@ -2,7 +2,6 @@ class CreateDailyTotals < ActiveRecord::Migration
   def change
     create_table :daily_totals do |t|
       t.integer :count, null: false
-      t.decimal :most_recent_tweet, null: false
       t.boolean :error, default: false, null: false
       t.integer :company_id, null: false
       t.integer :day_id, null: false
