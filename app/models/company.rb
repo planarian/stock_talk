cf2d62aedca5ceb62adcd4a99b5b4f5361a670ca
Jay <jhd@fastmail.net>
2015-04-09 00:21:32 -0400

    Update validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,16 @@
 class Company < ActiveRecord::Base
   has_many :daily_totals
+  has_many :share_prices
   has_many :days, :through => :daily_totals
-  has_many :queries
 
-  validates :name, presence: true
+  validates :name, :symbol, presence: true
+
+  
+  def self.list_yahoo_format
+    out = ""
+    all.each { |company| out << "\"#{company.symbol}\"," }
+    out.chop
+  end
 
 
 end
