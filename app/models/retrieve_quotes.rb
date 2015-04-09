b0087cf03c193b7cb4715ceb9e3f735e7840aa20
Jay <jhd@fastmail.net>
2015-04-09 16:16:20 -0400

    Fix error handling

--- app/models/retrieve_quotes.rb
+++ app/models/retrieve_quotes.rb
@@ -1,6 +1,10 @@
 module RetrieveQuotes
   Date.send(:include, CoreExtensions::Date::Yahoo)
 
+  def self.get_yesterday
+    get(Date.today - 1)
+  end
+
   def self.get(day)
     day_of = Day.find_by(date: day)
     print "Downloading quotes for #{day_of.date}..."
@@ -12,12 +16,14 @@ module RetrieveQuotes
     end
   end
 
-
   def self.ask(day_of)
     from_server = Typhoeus.get(prepare_url(day_of.date))
     if from_server.success?
-      parsed  = JSON.parse(from_server.body)["query"]["results"]
-      results = parsed ? parsed["quote"] : :closed
+      parsed  = JSON.parse(from_server.body)
+      unless parsed[:error]
+        market_open = !!parsed["query"]["results"]
+        results = market_open ? parsed["query"]["results"]["quote"] : :closed
+      end
     end
     results
   end
@@ -25,15 +31,19 @@ module RetrieveQuotes
   def self.store(day_of)
     results = ask(day_of)
     Company.all.each_with_index do |company, i|
-      unless SharePrice.find_by(company_id: company, day_id: day_of)
-        init = {company_id: company.id, day_id: day_of.id}
-        # Why not 'company_id: company, day_id: day_of'?
-        if results.respond_to?(:[])
-          init.merge!(price: results[i]["Close"].to_d)  
-        elsif results.nil?
-          init.merge!(error: true)
-        end
+      init = {company_id: company.id, day_id: day_of.id}
+      # Why not 'company_id: company, day_id: day_of'?
+      if results.instance_of?(Array)
+        init.merge!(price: results[i]["Close"].to_d, error: false)  
+      elsif results.nil?
+        init.merge!(error: true)
+      end
+      
+      existing_record = SharePrice.find_by(company_id: company, day_id: day_of)
+      unless existing_record
         SharePrice.create!(init)
+      else
+        existing_record.update(init)
       end
     end
     results
