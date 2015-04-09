8a8d3044dfbbd28c2290b8a83f1e143b052b048c
Jay <jhd@fastmail.net>
2015-04-09 00:22:38 -0400

    Update validations

--- app/models/share_price.rb
+++ app/models/share_price.rb
@@ -2,5 +2,6 @@ class SharePrice < ActiveRecord::Base
   belongs_to :company
   belongs_to :day
 
-  validates :day_id, :company_id, :error, presence: true
+  validates :error, exclusion: { in: [nil] }
+  validates :day_id, :company_id, presence: true
 end
