2c7ebd92c7516a00eee011e6010bb216d6196fac
Jay <jhd@fastmail.net>
2015-04-09 00:23:23 -0400

    Add autoload for lib/

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module StockTalk
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.autoload_paths += %W(#{config.root}/lib)
+
     config.generators do |generate|
         generate.helper false
         generate.assets false
