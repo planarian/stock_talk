d3ba3baa325c782d0c9d9b851a514625926bea3b
Jay <jhd@fastmail.net>
2015-04-09 16:13:02 -0400

    Add .add

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,4 +3,14 @@ class Day < ActiveRecord::Base
   has_many :share_prices
   has_many :companies, :through => :daily_totals
   validates :date, presence: true
+
+  def self.add(num_days, offset = 0)
+    unless last.nil?
+      last_day = last.date
+    else
+      last_day = create!(date: Date.today - 1)
+      offset = 1
+    end
+      (1..(num_days - offset)).each { |i| create!(date: last_day + i) }
+  end
 end
