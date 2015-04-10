9c8d8e5078f01d83e088edca391a8fcfd3d256c5
Jay <jhd@fastmail.net>
2015-04-10 12:03:38 -0400

    Associate days and companies through share prices

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,6 +2,8 @@ class Day < ActiveRecord::Base
   has_many :daily_totals
   has_many :share_prices
   has_many :companies, :through => :daily_totals
+  #rename above to :companies_with_tweets
+  has_many :companies_with_shares, :through => :share_prices, :source => :company
   validates :date, presence: true
 
   def self.add(num_days, offset = 0)
