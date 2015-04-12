f153eddf1d912a6d4116dbedcb0946eeb9cd97e3
Jay <jhd@fastmail.net>
2015-04-12 15:26:32 -0400

    Add #price reader replacement

--- app/models/share_price.rb
+++ app/models/share_price.rb
@@ -4,4 +4,8 @@ class SharePrice < ActiveRecord::Base
 
   validates :error, exclusion: { in: [nil] }
   validates :day_id, :company_id, presence: true
+
+  def price
+    read_attribute(:price).to_f.round(2)
+  end
 end
