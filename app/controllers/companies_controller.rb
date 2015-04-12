4f3cdf8a023722b4d2d89d243c5a161ec90b0577
Jay <jhd@fastmail.net>
2015-04-12 15:25:42 -0400

    Add controller for displaying individual company data

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,15 @@
 class CompaniesController < ApplicationController
 
-def index
-end
-
+  def show
+    @current = Company.find(params[:id]).name
+    @record = []
+    
+    days = Day.includes(:daily_totals, :share_prices)
+    days.each do |this_day|
+      @record << { day: this_day.date, 
+                   tweets: this_day.daily_totals.find_by(company: params[:id]).count,
+                   share_price: this_day.share_prices.find_by(company: params[:id]).price
+                 }
+    end
+  end
 end
