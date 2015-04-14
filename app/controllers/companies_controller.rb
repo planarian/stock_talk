27feb39dca6a0859de0edbea3520c0b25aae1c2c
Jay <jhd@fastmail.net>
2015-04-13 22:19:21 -0400

    Add AJAX handling, set string formatting for date in hash output

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,14 +2,26 @@ class CompaniesController < ApplicationController
 
   def show
     @current = Company.find(params[:id]).name
-    @record = []
-    
-    days = Day.includes(:daily_totals, :share_prices)
-    days.each do |this_day|
-      @record << { day: this_day.date, 
-                   tweets: this_day.daily_totals.find_by(company: params[:id]).count,
-                   share_price: this_day.share_prices.find_by(company: params[:id]).price
-                 }
+    respond_to do |format|
+      format.json do
+        render json: get_record
+      end
+      format.any do
+        render :show
+      end
     end
   end
+
+  def get_record
+    record = []
+    days = Day.includes(:daily_totals, :share_prices)
+    days.each do |this_day|
+      record << { day: this_day.date.strftime('%b %-d'),
+                  tweets: this_day.daily_totals.find_by(company: params[:id]).count,
+                  share_price: this_day.share_prices.find_by(company: params[:id]).price
+                 }
+    end
+    record
+  end
+
 end
