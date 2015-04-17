4694cf68d74d0534d2fbb1aef0e8ccb8f401ac0b
Jay <jhd@fastmail.net>
2015-04-17 08:37:05 -0400

    Add #price_str, update #csv_row to use it

--- app/models/share_price.rb
+++ app/models/share_price.rb
@@ -10,11 +10,15 @@ class SharePrice < ActiveRecord::Base
     p ? p.to_f.round(2) : nil
   end
 
+  def price_str
+    sprintf("%.2f", price)
+  end
+
   def self.csv_header
     "Date, Closing\n"
   end
 
   def csv_row
-    "#{day.date.yahoo_format}, #{error ? nil : price}\n"
+    "#{day.date.yahoo_format}, #{error ? nil : price_str}\n"
   end
 end
