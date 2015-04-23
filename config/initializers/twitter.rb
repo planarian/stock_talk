bc1fc9aadad64f9463bd356da92a9076862fe9ad
Jay <jhd@fastmail.net>
2015-04-23 16:40:05 -0400

    Add proxy support for Heroku's QuotaGuard add-on

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,15 @@
+proxy_uri = URI(ENV["QUOTAGUARD_URL"])
+
+proxy = {
+ uri: "#{proxy_uri.host}:#{proxy_uri.port}",
+ user: proxy_uri.user, 
+ password: proxy_uri.password
+}
+
 ::Client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["consumer_key"]
   config.consumer_secret     = ENV["consumer_secret"]
   config.access_token        = ENV["access_token"]
   config.access_token_secret = ENV["access_token_secret"]
+  config.proxy = proxy
 end
